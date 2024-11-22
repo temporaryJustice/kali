620bf30d8d3c96dda4e969af523ab933dd5571f7
temporaryJustice <temporaryjustice@outlook.com>
2024-11-22 15:03:07 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -37,7 +37,7 @@ curl https://sliver.sh/install | sudo bash
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
-export PATH="$PATH:/opt/nvim-linux64/bin"
+echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> ~/.zshrc
 source ~/.zshrc
 git clone -b v2.0 https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 
 rm nvim-linux64.tar.gz
